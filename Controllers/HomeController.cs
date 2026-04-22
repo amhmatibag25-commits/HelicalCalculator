b22ee75b91f11f2c05e54479fa443aac9ffae5dc
amhmatibag25-commits <amhmatibag25@bpsu.edu.ph>
2026-04-22 14:29:03 +0800

    Refactor Calculate method for volume calculation
    Updated the Calculate method to use new parameters for volume calculation and added height, major radius, and minor radius to ViewBag.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,24 +9,20 @@ namespace HelicalToroidFrustum.Controllers
             return View();
         }
 
+
         [HttpPost]
-public IActionResult Calculate(double R_major, double r_minor, double theta)
-{
-    // R_major = distansya mula sa gitna ng donut hanggang sa gitna ng tube
-    // r_minor = radius ng mismong tube
-    // theta = gaano kalaking parte ng ikot ang kinuha (in degrees)
+        public IActionResult Calculate(double h, double R, double r)
+        {
+            double volume = (Math.PI * h / 3.0) * (Math.Pow(R, 2) + (R * r) + Math.Pow(r, 2));
 
-    // Convert degrees to radians
-    double radians = theta * (Math.PI / 180.0);
+            ViewBag.Result = volume.ToString("F4");
+            ViewBag.Height = h;
+            ViewBag.MajorR = R;
+            ViewBag.MinorR = r;
 
-    // Formula: Volume = (Area of circle) * (Arc length of the center path)
-    // V = (π * r^2) * (R * theta_radians)
-    double volume = (Math.PI * Math.Pow(r_minor, 2)) * (R_major * radians);
+            return View("Index");
+        }
 
-    ViewBag.Result = volume.ToString("F4");
-    
-    return View("Index");
-}
 
     }
 }
