9141cbb292f756c21e1b487ff300ee410f56211a
amhmatibag25-commits <amhmatibag25@bpsu.edu.ph>
2026-04-22 14:34:09 +0800

    Refactor Calculate method in HomeController
    Updated the Calculate method to use new parameters for volume calculation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,25 +10,18 @@ namespace HelicalToroidFrustum.Controllers
         }
 
 
-[HttpPost]
-public IActionResult Calculate(double R_major, double r_minor, double theta)
-{
-    // R_major = distansya mula sa gitna ng donut hanggang sa gitna ng tube
-    // r_minor = radius ng mismong tube
-    // theta = gaano kalaking parte ng ikot ang kinuha (in degrees)
+        [HttpPost]
+        public IActionResult Calculate(double h, double R, double r)
+        {
+            double volume = (Math.PI * h / 3.0) * (Math.Pow(R, 2) + (R * r) + Math.Pow(r, 2));
 
-    // Convert degrees to radians
-    double radians = theta * (Math.PI / 180.0);
+            ViewBag.Result = volume.ToString("F4");
+            ViewBag.Height = h;
+            ViewBag.MajorR = R;
+            ViewBag.MinorR = r;
 
-    // Formula: Volume = (Area of circle) * (Arc length of the center path)
-    // V = (π * r^2) * (R * theta_radians)
-    double volume = (Math.PI * Math.Pow(r_minor, 2)) * (R_major * radians);
-
-    ViewBag.Result = volume.ToString("F4");
-    
-    return View("Index");
-}
-
+            return View("Index");
+        }
 
 
     }
